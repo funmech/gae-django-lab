297cab1f3fb311726a510b73f3dc8425bded7f68
Jianfeng Li <jianfeng.li@pwc.com>
2019-03-15 16:13:20 +1030

    One basic default view

--- lab/app1/views.py
+++ lab/app1/views.py
@@ -1,4 +1,15 @@
+import datetime
+import time
+
+from django.utils import timezone
 from django.http import HttpResponse
 
+
+def current_datetime():
+    now = datetime.datetime.now()
+    html = "<html><body>On server, now it is %s (%f), tz = %s.</body></html>" % (now, time.time(), timezone.now())
+    return HttpResponse(html)
+
+
 def index(request):
-    return HttpResponse('good')
+    return current_datetime()
